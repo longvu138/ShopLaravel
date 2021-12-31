7b205a5a8bbb1ae3363aa00a5370a319b460438a
longvu1308 <tranlongvu138@gmail.com>
2021-12-31 19:20:07 +0700

    CRUD Slider

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\MainController;
 use App\Http\Controllers\Admin\MenuController;
 use App\Http\Controllers\admin\ProductController;
+use App\Http\Controllers\admin\SliderController;
 use App\Http\Controllers\Admin\UploadController;
 use App\Http\Controllers\Admin\User\LoginController;
 use Illuminate\Support\Facades\Route;
@@ -52,6 +53,17 @@ use Illuminate\Support\Facades\Route;
             // bắt id của để get ra thông tin
             Route::get('edit/{product}',[ProductController::class,'show']);
             Route::post('edit/{product}',[ProductController::class,'update']);
+        });
+
+
+         # ROUTE SLIDER
+         Route::prefix('sliders')->group(function () {
+            Route::get('add', [SliderController::class, 'create']);
+            Route::post('add', [SliderController::class, 'store']);
+            Route::get('list', [SliderController::class, 'index']);
+            Route::get('edit/{slider}', [SliderController::class, 'show']);
+            Route::post('edit/{slider}', [SliderController::class, 'update']);
+            Route::DELETE('destroy', [SliderController::class, 'destroy']);
         });
 
 
