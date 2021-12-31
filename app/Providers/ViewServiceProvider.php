1d75e0d08212c8c177a27ed25c4e720ec075f2b2
longvu1308 <tranlongvu138@gmail.com>
2021-12-31 21:39:45 +0700

    tao viewProvider share menu

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\View\Composers\MenuComposer;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // gửI sang view name (frontend.header)
+        View::composer('frontend.header', MenuComposer::class);
+    
+    }
+}
