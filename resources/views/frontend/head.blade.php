188dbbe68478195f076c9915b5154f2e29ea91e0
longvu1308 <tranlongvu138@gmail.com>
2021-12-31 20:01:26 +0700

    giao dien admin

---
+++ resources/views/frontend/head.blade.php
@@ -0,0 +1,33 @@
+<title>{{$title}}</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="/template/frontend/images/icons/favicon.png"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/template/frontend/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/template/frontend/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/template/frontend/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/template/frontend/fonts/linearicons-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/template/frontend/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="/template/frontend/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/template/frontend/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/template/frontend/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="/template/frontend/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/template/frontend/vendor/slick/slick.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/template/frontend/vendor/MagnificPopup/magnific-popup.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/template/frontend/vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/template/frontend/css/util.css">
+	<link rel="stylesheet" type="text/css" href="/template/frontend/css/main.css">
+<!--===============================================================================================-->
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\admin\ProductController;
 use App\Http\Controllers\admin\SliderController;
 use App\Http\Controllers\Admin\UploadController;
 use App\Http\Controllers\Admin\User\LoginController;
+use App\Http\Controllers\Frontend\MainController as FrontendMainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -70,10 +71,11 @@ use Illuminate\Support\Facades\Route;
          #Route Upload
          Route::post('/upload/services', [UploadController::class, 'store']);
 
-
-
+      
     });
   
+  #Route frontend
+  Route::get('/',[FrontendMainController::class,'index']);
 
  });
 
